93ed3ed7b3548d36c0ab3dd2952f7ca8890397ed
Alex Santana <aleximbw@gmail.com>
2025-04-01 22:37:34 -0300

    Inicio do back-end
    Estruturação do back-end e inicio da integração com o back-end para fazer a chamado de callback.

--- nwm/app.py
+++ nwm/app.py
@@ -1,13 +1,34 @@
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
+from network_monitor import get_network_traffic
 
-
+#INSTANCIAMENTO DO APP DASH
 app = Dash(__name__)
 
+#DEFINE O TITULO DA PAGINA
+app.title="NWM"
+
+
+#ÁREA DE CONFIGURAÇÃO DO FRONT-END
 app.layout = html.Div([
     html.H1("Monitor de Tráfego de Rede"),
-    dcc.Graph(id="grafico-trafego"),
+    #GRAFICO PAUSADO
+    #dcc.Graph(id="grafico-trafego"),
+
+    #TESTE DE FUNCIONAMENTO DA COLETA DE PACOTES
+    html.Div([
+        html.P("Bytes Enviados"),
+        html.Div(id="bytes-enviados")
+    ]),
+    html.Div([
+        html.P("Bytes Recebidos:"),
+        html.Div(id="bytes-recebidos"),
+    ]),
+
     dcc.Interval(id="intervalo-atualizacao", interval=2000) #Atualiza a cada 2s
 ])
 
+
+#FAZ O SERVIDOR RODAR NO NAVEGADOR
 if __name__ == "__main__":
     app.run()
---
+++ nwm/network_monitor.py
@@ -0,0 +1,7 @@
+import psutil
+
+def get_network_traffic():
+    #CAPTURA E RETORNA OS BYTES ENVIADOS E RECEBIDOS
+
+    net_io = psutil.net_io_counters()
+    return net_io.bytes_sents, net_io.bytes_recv
